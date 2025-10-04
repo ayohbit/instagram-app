697bd61c6d8de4e47c3fb2cb8bc62073530ce5f7
Lucas Araújo <73756840+ayohbit@users.noreply.github.com>
2025-10-04 17:26:29 -0300

    tela de login e validação do formulario
    inserido a tela de login com validação de email e senha.

---
+++ app/src/main/java/com/androidexpress/instagram_app/login/view/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.androidexpress.instagram_app.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androidexpress.instagram_app.R
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_login)
+
+    findViewById<TextInputLayout>(R.id.login_edit_email_input)
+      .error = "Esse e-mail é inválido"
+  }
+}
